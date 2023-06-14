5e7517769682550e72aac6c71b5ce4f3401f21d3
Simiyu-coder <simiyukelvin93@gmail.com>
2023-06-14 21:42:24 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const API_URL = 'http://www.omdbapi.com/apikey=81e1baca';
+
+const app = () => {
+    return (
+        <h1>App</h1>
+    )
+}
+
+export default App;
